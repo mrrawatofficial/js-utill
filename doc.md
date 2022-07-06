124f57d90355c20ba23d217be2d890dd127a9e61
Rahul Rawat <rahulrawat520@gmail.com>
2022-07-06 15:44:11 +0530

    Initial commit

---
+++ doc.md
@@ -0,0 +1,43 @@
+## set cookie
+
+const { setCookie, getCookie, deleteCookie } = mbrCookies();
+
+pass object
+setCookies({
+'name': 'value', || string
+'value': 'value2', || any
+'expire': 1 || int days(default: 1)
+})
+
+## get cookie
+
+getCookie("kub"); || name of the cookie || string || will return the value of the cookie if it exists or null if it doesn't
+
+## get cookie
+
+deleteCookie("kub"); || name of the cookie || string || will delete the cookie if it exists
+
+## copyToClipboard
+
+<script>
+    const { copyToClipboard } = mbrClipboard();
+    const input = document.querySelector('[data-input="copy"]');
+    const button = document.querySelector('[data-button="copy"]');
+    button.addEventListener("click", () => {
+        copyToClipboard(input.value);
+    });
+</script>
+
+copyToClipboard("text"); || string || will copy the text to the clipboard
+
+## websiteLoader
+
+const options = {
+image: gifURL || string || url of the gif to load, default spinner,
+bg: "#333" || string || background color, default #333,
+bgOpacity: 0.5 || int 0 to 1 || background opacity, default 0.5,
+size: 100 || int || size of the loader, default 100px,
+}
+const loader = websiteLoader(options);
+loader.init();
+loader.destroy();
---
+++ src/ts/index.ts
@@ -0,0 +1,106 @@
+type setCookieType = {
+	name: string;
+	value: any;
+	expire?: number;
+};
+type loaderType = {
+	image?: string;
+	bg?: string;
+	bgOpacity?: number;
+	size?: number;
+};
+
+// for cookies
+const mbrCookies = () => {
+	// set cookie
+	const setCookie = (data: setCookieType) => {
+		const { name, value, expire = 1 } = data;
+		const date = new Date();
+		date.setTime(date.getTime() + expire * 24 * 60 * 60 * 1000);
+		document.cookie = `${name}=${JSON.stringify(
+			value
+		)};expires=${date.toUTCString()}`;
+	};
+	// get cookie
+	const getCookie = (name: string) => {
+		const cookies = document.cookie.split(";");
+		for (let i = 0; i < cookies.length; i++) {
+			const cookie = cookies[i].split("=");
+			if (cookie[0].trim() === name) {
+				return JSON.parse(cookie[1]);
+			}
+		}
+		return null;
+	};
+	// delete cookie
+	const deleteCookie = (name: string) => {
+		if (getCookie(name)) {
+			const date = new Date();
+			date.setTime(date.getTime() - 1);
+			document.cookie = `${name}=;expires=${date.toUTCString()}`;
+		}
+	};
+	return {
+		setCookie,
+		getCookie,
+		deleteCookie,
+	};
+};
+
+// copy to clipboard
+const mbrClipboard = () => {
+	const copyToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text);
+	};
+	const cutToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text);
+	};
+	return {
+		copyToClipboard,
+	};
+};
+
+// website loader for loading a website
+const mbrLoader = (data: loaderType) => {
+	const { image, bg = "#333", bgOpacity = 0.5, size = 100 } = data;
+	const wesite_inner_loader = `<div style="width:${size}px; height:${size}px; background:${bg}; border:5px solid transparent; border-top:5px solid white; border-bottom:5px solid white; border-radius:50%;  animation:spinn 1s linear infinite;"></div>`;
+	const loader_img = `<img src="${image}" style="width:${size}px; height:${size}px;">`;
+	const website_loader = `<div id="mbr_loader" style="background:${bg}; display:flex; width:100%; min-height:100vh; justify-content:center; align-items:center; opacity:${bgOpacity}">${
+		image ? loader_img : wesite_inner_loader
+	}</div>`;
+	const init = function () {
+		document.body.insertAdjacentHTML("afterbegin", website_loader);
+		spinn();
+	};
+	const destroy = function () {
+		document.querySelector("#mbr_loader")?.remove();
+	};
+	const spinn = () => {
+		let dynamicStyles = null as any;
+		function addAnimation(body: string) {
+			if (!dynamicStyles) {
+				dynamicStyles = document.createElement("style");
+				dynamicStyles.type = "text/css";
+				document.head.appendChild(dynamicStyles);
+			}
+
+			dynamicStyles.sheet.insertRule(body, dynamicStyles.length);
+		}
+
+		addAnimation(`
+		  @keyframes spinn { 
+			from{
+				transform:rotate(0deg);
+			  }
+			  to{
+				transform:rotate(360deg);
+			  }
+		  }
+		`);
+	};
+
+	return {
+		init,
+		destroy,
+	};
+};
